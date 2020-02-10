1fca942ca0fa70bcebf2996b2a77ae57c0d9d87d
Pablo C. Ortiz G <pablo.orgz@gmail.com>
2020-02-09 23:28:57 -0600

    changed my mind, restructured things - flappy in progress

---
+++ LearningMultiple/Assets/MoveLeft.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 0f;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ LearningMultiple/Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float upVelocity = 0f;
+
+    private Rigidbody2D rb;
+    private bool goingUp;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        goingUp = Input.GetMouseButton (0);
+    }
+
+    private void FixedUpdate()
+    {
+        if (goingUp)
+        {
+            rb.velocity = Vector2.up * upVelocity * Time.deltaTime;
+        }
+    }
+}
