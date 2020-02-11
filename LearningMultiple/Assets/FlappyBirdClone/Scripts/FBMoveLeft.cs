65aef65068819586078e2d72dda126a32ff53c91
Pablo C. Ortiz G <pablo.orgz@gmail.com>
2020-02-10 23:04:17 -0600

    re-structured files inside game folder

---
+++ LearningMultiple/Assets/FlappyBirdClone/Scripts/FBMoveLeft.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBMoveLeft : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 0f;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ObstacleTrigger"))
+        {
+            // TODO: Deactivate/return to stack/remove
+        }
+    }
+}
